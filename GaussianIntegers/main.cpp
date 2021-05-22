b348852b33a75307b25ee20ac500002354e8d0de
theMathlad <thesciencer@protonmail.com>
2021-05-22 14:39:42 +0200

    Updated main with examples

--- GaussianIntegers/main.cpp
+++ GaussianIntegers/main.cpp
@@ -7,12 +7,18 @@ int main()
 {
     GI alpha {1, 2};
     GI beta {-3, 5};
-    GI gamma {1284, -418764};
     cout << alpha + beta << endl;
     cout << alpha - beta << endl;
     cout << alpha * beta << endl;
-    cout << GIgcd(alpha, beta) << endl;
-    cout << gamma << " = "; printGIproduct(GIprimefactor(gamma));
-    cout << QuarticResSymbol(gamma, alpha);
+    cout << beta / alpha << endl;
+    cout << GIgcd({347, 89}, {117, -547}) << endl;
+    vector<GI> v = GIexgcd({347, 89}, {117, -547});
+    for (GI a : v)
+        cout << a << endl;
+    cout << GImodlinearsolve({347, 89}, {117, -547}, {2003}) << endl;
+    GImodlinearsolve({347, 89}, {117, -547}, {1, 2});
+    printGIproduct(GIprimefactor({1284, -418764}));
+    cout << QuarticResSymbol({347, 89}, {5, 6}) << endl;
+    cout << isGIprime({5, 6});
     return 0;
 }
